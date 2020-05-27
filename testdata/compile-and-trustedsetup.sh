f80cec2e113f33228ee3b6beed3ee062adbf29d3
arnaucube <root@arnaucube.com>
2020-05-27 13:09:22 +0200

    Update testdata to use local snarkjs & circom

--- testdata/compile-and-trustedsetup.sh
+++ testdata/compile-and-trustedsetup.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+npm install
+
 compile_and_ts() {
   echo $(date +"%T") "circom circuit.circom --r1cs --wasm --sym"
   itime="$(date -u +%s)"
-  circom circuit.circom --r1cs --wasm --sym
+  ../node_modules/.bin/circom circuit.circom --r1cs --wasm --sym
   ftime="$(date -u +%s)"
   echo "	($(($(date -u +%s)-$itime))s)"
 
   echo $(date +"%T") "snarkjs info -r circuit.r1cs"
-  snarkjs info -r circuit.r1cs
+  ../node_modules/.bin/snarkjs info -r circuit.r1cs
 
   echo $(date +"%T") "snarkjs setup"
   itime="$(date -u +%s)"
-  snarkjs setup
+  ../node_modules/.bin/snarkjs setup
   echo "	($(($(date -u +%s)-$itime))s)"
   echo $(date +"%T") "trusted setup generated"
 
@@ -21,7 +23,7 @@ compile_and_ts() {
 
   echo $(date +"%T") "snarkjs generateverifier"
   itime="$(date -u +%s)"
-  snarkjs generateverifier
+  ../node_modules/.bin/snarkjs generateverifier
   echo "	($(($(date -u +%s)-$itime))s)"
   echo $(date +"%T") "generateverifier generated"
 }
@@ -29,7 +31,11 @@ compile_and_ts() {
 echo "compile & trustesetup for circuit1"
 cd circuit1
 compile_and_ts
+
 echo "compile & trustesetup for circuit2"
 cd ../circuit2
 compile_and_ts
 
+echo "compile & trustesetup for circuit3"
+cd ../circuit3
+compile_and_ts
